d3da0b1cabbc4e0140fb5de6c1cd62031d627a20
mac <mac@MUKHTERS-MACBOOK.local>
2023-07-31 00:31:05 +0600

    complete category update and delete api

--- api/controllers/categoryController.js
+++ api/controllers/categoryController.js
@@ -3,14 +3,12 @@ const Category = require('./../models/Category')
 // all the categories show from database || controller
 const categories = async (req, res, next)=> {
     const categoriesData = await Category.find();
-
     res.status(200).json(categoriesData);
 }
 
 // single category add to the database || controller
 const addCategory = async (req, res, next)=> {
     const categoryData = req.body;
-
     const newCategory = new Category(categoryData);
     const result = await newCategory.save();
 
@@ -20,7 +18,26 @@ const addCategory = async (req, res, next)=> {
     });
 }
 
+// update category || controller
+const updateCategory = async (req, res, next)=> {
+    const updateData = req.body;
+    const categoryId = req.params.id;
+
+    const result = await Category.updateOne({ _id: categoryId }, updateData);
+    res.status(200).json({dataUpdated: true, result});
+}
+
+// delete category || controller
+const deleteCategory = async (req, res, next)=> {
+    const categoryId = req.params.id;
+    const deletedData = await Category.deleteOne({_id: categoryId});
+    res.status(200).json({dataDeleted: true, deletedData});
+}
+
+
 module.exports = {
     categories,
-    addCategory
+    addCategory,
+    updateCategory,
+    deleteCategory
 }
--- api/routers/categoryRouter.js
+++ api/routers/categoryRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // all internal imports
-const {categories, addCategory} = require("../controllers/categoryController");
+const {categories, addCategory, updateCategory, deleteCategory} = require("../controllers/categoryController");
 
 
 // all the categories list show router
@@ -10,6 +10,12 @@ router.get("/", categories);
 
 // add a category router
 router.post("/", addCategory);
+
+// category update router
+router.patch("/:id", updateCategory);
+
+// category delete router
+router.delete("/:id", deleteCategory);
 
 
 module.exports = router;
